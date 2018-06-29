74f90f8420214ecf54936734beabe50b1eb0a8dd
AudreyUWU <40180816+AudreyUWU@users.noreply.github.com>
2018-06-29 11:39:37 -0400

    Create c4.js

---
+++ c4.js
@@ -0,0 +1,164 @@
+let map = [
+    ["", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", ""]
+]
+
+function resetGame() {
+    let board = document.getElementById("mazeDiv");
+    board.innerHTML = "";
+    map = [
+        ["", "", "", "", "", "", ""],
+        ["", "", "", "", "", "", ""],
+        ["", "", "", "", "", "", ""],
+        ["", "", "", "", "", "", ""],
+        ["", "", "", "", "", "", ""],
+        ["", "", "", "", "", "", ""]
+    ]
+    currentPlayer = "black"
+    drawBoard();
+}
+
+map.className = "map"
+
+const black = document.getElementById("black")
+const red = document.getElementById("red");
+const mazeDiv = document.getElementById("mazeDiv");
+let currentPlayer = "black";
+
+function addDisc(column) {
+    for (let i = 5; i >= 0; i--) {
+        if (map[i][column] == "") {
+            if (currentPlayer == "black") {
+                map[i][column] = black;
+                let test = document.getElementById("cell" + i + column);
+                test.innerHTML = '<img src="Black_Circle.png" class="disc">'
+                checkForWinHorizontal();
+                checkForWinVertical();
+                checkForWinDDown();
+                checkForWinDUp();
+
+                currentPlayer = "red";
+                return
+            } else if (currentPlayer == "red") {
+                map[i][column] = red;
+                let test = document.getElementById("cell" + i + column);
+                test.innerHTML = '<img src= "Red_Circle.png" class="disc">'
+                checkForWinHorizontal();
+                checkForWinVertical();
+                checkForWinDDown();
+                checkForWinDUp()
+                currentPlayer = "black";
+                return
+            }
+        }
+    }
+}
+
+
+function drawBoard() {
+    let storage = [];
+    for (let row = 0; row < map.length; row++) {
+        const rowStr = map[row];
+        const rowDiv = document.createElement("div");
+        rowDiv.className = "row";
+
+        let x = [];
+        storage.push([]);
+
+        for (let i = 0; i < rowStr.length; i++) {
+            let cellClass = rowStr[i];
+            const cellDiv = document.createElement("div");
+
+            cellDiv.className = "cell " + cellClass;
+            cellDiv.id = "cell" + row + i
+
+            storage[row].push(cellDiv);
+            rowDiv.appendChild(cellDiv);
+        }
+        mazeDiv.appendChild(rowDiv);
+    }
+
+    handleClick = function (event) {
+        var column = event.target.value;
+        addDisc(column);
+    }
+
+    for (let i = 0; i <= 6; i++) {
+        var button = document.createElement("button");
+        button.className = "button"
+        button.value = i
+        button.textContent = 'drop';
+        button.addEventListener("click", handleClick);
+        mazeDiv.appendChild(button);
+    }
+
+    let restart = document.createElement("button")
+    restart.textContent = "click me to restart"
+    restart.onclick = function () {
+        resetGame();
+    }
+
+    mazeDiv.appendChild(restart);
+}
+
+drawBoard();
+
+function checkForWinHorizontal() {
+    for (let i = 0; i < map.length; i++) {
+        for (let j = 0; j < 7; j++) {
+            var cell = map[i][j]
+            if (cell !== "") {
+                if (map[i][j] && map[i][j+1] && map[i][j+2] && map[i][j+3] && map[i + 1][j] && cell === map[i][j + 1] && cell === map[i][j + 2] && cell === map[i][j + 3]) {
+                    console.log(currentPlayer + " wins horizontal!")
+                    resetGame();
+                }
+            }
+        }
+    }
+}
+
+function checkForWinVertical() {
+    for (let i = 0; i < map.length; i++) {
+        for (let j = 0; j < 7; j++) {
+            var cell = map[i][j]
+            if (cell !== "") {
+                if (map[i + 1] && map[i + 2] && map[i + 3] && map[i + 1][j] && cell === map[i + 1][j] && cell === map[i + 2][j] && cell === map[i + 3][j]) {
+                    console.log(currentPlayer + " wins vertical!")
+                    resetGame();
+                }
+            }
+        }
+    }
+}
+
+function checkForWinDDown() {
+    for (let i = 0; i < map.length; i++) {
+        for (let j = 0; j < 7; j++) {
+            let cell = map[i][j]
+            if (cell !== "") {
+                if (map[i - 1] && map[i - 2] && map[i - 3] && cell === map[i - 1][j - 1] && cell === map[i - 2][j - 2] && cell === map[i - 3][j - 3]) {
+                    console.log(currentPlayer + " wins ddown!")
+                    resetGame();
+                }
+            }
+        }
+    }
+}
+
+function checkForWinDUp() {
+    for (let i = 0; i < map.length; i++) {
+        for (let j = 0; j < 7; j++) {
+            let cell = map[i][j]
+            if (cell !== "") {
+                if (map[i + 1] && map[i + 2] && map[i + 3] && cell === map[i + 1][j - 1] && cell === map[i + 2][j - 2] && cell === map[i + 3][j - 3]) {
+                    console.log(currentPlayer + " wins dup!")
+                    resetGame();
+                }
+            }
+        }
+    }
+}
